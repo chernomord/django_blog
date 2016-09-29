9507084de81786a4b46d43cb89adbe2a1b1ec95d
Slava <horlet@gmail.com>
2016-09-29 20:59:00 +0300

    (chore) pre-production

--- myblog/settings.py
+++ myblog/settings.py
@@ -148,8 +148,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Production settings
 
-DATABASES['default'] = dj_database_url.config()
-
 # DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
